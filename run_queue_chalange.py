56a56c7812c5fc28a451c991662758c50e127c0b
Ronald Johnson <lightarrow@icloud.com>
2022-01-23 16:32:27 -0600

    Queues challenge

--- run_queue_chalange.py
+++ run_queue_chalange.py
@@ -2,5 +2,5 @@ from queue_challenge import Job, PrintQueue, Printer
 my_queue = PrintQueue()
 my_job = Job()
 my_printer = Printer()
-my_queue.enqueue('apple')
-my_job.print_page()
+my_queue.enqueue(my_job)
+my_printer.print_job(my_queue)
