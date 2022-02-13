c537cbca7b5f38aeb7f8e248ddacd019da487d66
lightarrow <lightarrow@icloud.com>
2022-02-13 12:49:27 -0600

    update sunday

---
+++ cat_eof.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+cat <<EOF > start.sh
+git config --global user.name ${name}
+git config --global user.email ${email} 
+git clone git@github.com:lightarrow/auto-deploy.git
+echo "# auto-deploy" >> README.md
+git init
+git add README.md
+message='starting'
+git commit -m "${message}"
+git branch -M main
+git remote add origin git@github.com:lightarrow/auto-deploy.git
+git push --set-upstream origin main
+git remotes -v 
+# git clone git@github.com:lightarrow/auto-deploy.git --depth 10
+EOF
