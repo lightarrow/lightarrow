90a9fb1409ff7a39257b6f00da9283b80a1d8633
lightarrow <lightarrow@icloud.com>
2022-02-13 13:02:52 -0600

    man

--- cat_eof.sh
+++ cat_eof.sh
@@ -13,4 +13,11 @@ git remote add origin git@github.com:lightarrow/auto-deploy.git
 git push --set-upstream origin main
 git remotes -v 
 # git clone git@github.com:lightarrow/auto-deploy.git --depth 10
+#git add cat_eof.sh
+#add ssh key on new session. 
+# make sure its running
+eval "$(ssh-agent -s)"
+#add key
+ssh-add ~/.ssh/my_private_key 
+#promts for key passphrase
 EOF
--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
-git config --global user.name ${name}
-git config --global user.email ${email} 
+git config --global user.name 
+git config --global user.email  
 git clone git@github.com:lightarrow/auto-deploy.git
 echo "# auto-deploy" >> README.md
 git init
 git add README.md
 message='starting'
-git commit -m "${message}"
+git commit -m ""
 git branch -M main
 git remote add origin git@github.com:lightarrow/auto-deploy.git
 git push --set-upstream origin main
-
-
+git remotes -v 
+# git clone git@github.com:lightarrow/auto-deploy.git --depth 10
+#
