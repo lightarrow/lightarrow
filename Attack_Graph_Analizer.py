d4bcd9cd35248d6c098a9291cce64ae3cd17f09a
ronald johnson <lightarrow@icloud.com>
2025-09-22 12:18:33 -0500

    Vector classes laid out.

--- Attack_Graph_Analizer.py
+++ Attack_Graph_Analizer.py
@@ -4,6 +4,9 @@ Author: Ronald Johnson
 Date: 9-22-2025
 License: GPLv3
 link: https://www.gnu.org/licenses/quick-guide-gplv3.html
+This code takes CVSS version 3.x vectors and computes them.
+The vectors are then matched against host data to determine weight and priority.
+The risk will be calculated once the host data has been calculated.
 """
 class AttackVector:
     def __init__(self,physical, local, adjacent, network):
@@ -23,6 +26,49 @@ class PrivilegesRequired:
         self.high = 0.27
         self.no_auth = 0.85
 
-class UserInteractonMetric
+class UserInteractonMetric:
+    def __init__(self,no_interaction, required_interaction):
+        self.no_interaction = 0.85
+        self.required_interaction = 0.62
+
+
+class ConfidentialityImpactMetric:
+    def __init__(self,low, high):
+        self.no_impact = 0.00
+        self.low = 0.22
+        self.high = 0.56
+
+class AvailabilityImpactMetric:
+    def __init__(self,low, high):
+        self.no_impact = 0.00
+        self.low = 0.22
+        self.high = 0.56
+
+class Scope:
+    def __init__(self,unchanged, changed):
+        self.changed = False
+        self.unchanged = False
+
+class InterpertVector:
+    def __init__(self,vector, cvss_split):
+        self.vector = "CVSS:3.1/AV:A/AC:L/PR:N/UI:N/S:U/C:L/I:H/A:L"
+        self.cvss_split = None
+
+class KnownExploitedVulnerability:
+    def __init__(self,low, high):
+        self.exploited = False
 
 if __name__=='__main__':
+
+    while True:
+
+        try:
+            result =
+        except:
+            print('NAN error')
+            continue
+        else:
+            print('Thats OK')
+            break
+        finally:
+            print('This happens no matter what.')
