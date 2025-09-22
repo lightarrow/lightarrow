d343f192eea9e36d174ec27101d667588d55d8b4
ronald johnson <lightarrow@icloud.com>
2025-09-22 10:20:22 -0500

    CVSS analysis mapping.

---
+++ Attack_Graph_Analizer.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+"""
+Author: Ronald Johnson
+Date: 9-22-2025
+License: GPLv3
+link: https://www.gnu.org/licenses/quick-guide-gplv3.html
+"""
+Class record:
+    def __init__(self):
+        self.physical = 0.20
+        self.local = 0.55
+        self.adjacent = 0.62
+        self.network = 0.85
+
+
+
+
+if __name__=='__main__':
